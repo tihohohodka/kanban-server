37fc5eef16406856925da88c97a8bbea19791ac5
tihohohodka <kikboo1999@gmail.com>
2022-11-14 17:30:31 +0300

    2

--- Project management application/src/index.ts
+++ Project management application/src/index.ts
@@ -6,7 +6,7 @@ import * as serverService from './services/server.service';
 
 (async () => {
   try {
-    await mongoose.connect('mongodb+srv://BooleanN:securepass@cluster0.6zqhtbn.mongodb.net/?retryWrites=true&w=majority');
+    await mongoose.connect('mongodb+srv://BooleanN:securepass@cluster0.6zqhtbn.mongodb.net/Cluster0');
     serverService.server.listen(process.env.PORT || PORT, function () {
       console.log('Сервер ожидает подключения...');
     })
